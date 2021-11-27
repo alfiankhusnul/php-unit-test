8c1b63bc5e21b5491cacea5d54f7f9ea9fd1be0c
Alfian Khusnul Amar <alfiankhusnul@gmail.com>
2021-11-27 14:36:32 +0800

    merubah versi dan membuat test class

---
+++ tests/CounterTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AlfianKhusnul\Test;
+
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+
+class CounterTest extends TestCase
+{
+    public function testCounter()
+    {
+        $counter = new Counter();
+        $counter->increment();
+
+        Assert::assertEquals(1, $counter->getCounter());
+
+    }
+}
