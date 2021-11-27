778818522934137b9f8637118e05748298cd2ac8
Alfian Khusnul Amar <alfiankhusnul@gmail.com>
2021-11-27 14:51:48 +0800

    test dependency

--- tests/CounterTest.php
+++ tests/CounterTest.php
@@ -24,4 +24,21 @@ class CounterTest extends TestCase
 
         Assert::assertEquals(1, $counter->getCounter());
     }
+
+    public function testFirst():Counter
+    {
+        $counter = new Counter();
+        $counter->increment();
+        $this->assertEquals(1, $counter->getCounter());
+        return $counter;
+    }
+
+    /**
+     * @depends testFirst
+     */
+    public function testSecond(Counter $counter):void
+    {
+        $counter->increment();
+        $this->assertEquals(2, $counter->getCounter());
+    }
 }
