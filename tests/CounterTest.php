902d68e0bb09f751da4e9a378a941837975b824a
Alfian Khusnul Amar <alfiankhusnul@gmail.com>
2021-11-27 14:40:54 +0800

    membuat test dengan annotation

--- tests/CounterTest.php
+++ tests/CounterTest.php
@@ -13,6 +13,15 @@ class CounterTest extends TestCase
         $counter->increment();
 
         Assert::assertEquals(1, $counter->getCounter());
+    }
 
+    /**
+     * @test
+     */
+    public function otherTest(){
+        $counter = new Counter();
+        $counter->increment();
+
+        Assert::assertEquals(1, $counter->getCounter());
     }
 }
